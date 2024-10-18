5c060ac17ac4f2da81485ca4da03e0b6d81571ca
Jan "JoubaMety" Rašnovský <56211361+JoubaMety@users.noreply.github.com>
2024-10-18 22:07:16 +0200

    added multiple themes support! (part 3.1)
    bruh

---
+++ src/theme.js
@@ -0,0 +1,68 @@
+const themeTextMap = new Map([
+	["dark", "Catppuccin Mocha"],
+	["light", "Catppuccin Latte"],
+	["nord-dark", "Nord (Dark)"],
+	["nord-light", "Nord (Light)"],
+	["catppuccin-frappe", "Catppuccin Frappé"],
+	["catppuccin-macchiato", "Catppuccin Macchiato"],
+]);
+
+function earlyTheme() {
+	var storedTheme =
+		localStorage.getItem("theme") ||
+		(window.matchMedia("(prefers-color-scheme: dark)").matches
+			? "dark"
+			: "light");
+	if (storedTheme) {
+		document.documentElement.setAttribute("data-theme", storedTheme);
+	}	
+}
+
+function theme() {
+	var storedTheme =
+		localStorage.getItem("theme") ||
+		(window.matchMedia("(prefers-color-scheme: dark)").matches
+			? "dark"
+			: "light");
+	if (storedTheme) {
+		document.documentElement.setAttribute("data-theme", storedTheme);
+		changeThemeText(storedTheme);
+		addSelectedTheme(storedTheme);
+	}	
+}
+
+function changeTheme(targetTheme, ambiance) {
+	console.info("change theme to " + targetTheme + " (" + ambiance + ")");
+	if (ambiance == "dark") {
+		document.documentElement.classList.add("dark");
+	} else {
+		document.documentElement.classList.remove("dark");
+	}
+	document.documentElement.setAttribute("data-theme", targetTheme);
+	addSelectedTheme(targetTheme);
+	localStorage.setItem("theme", targetTheme);
+	changeThemeText(targetTheme);
+}
+
+function changeThemeText(targetTheme) {
+	document.getElementById("SelectedTheme").textContent = themeTextMap.get(targetTheme);
+}
+
+// reset theme
+function resetTheme() {
+    console.info("reset theme to default");
+	localStorage.removeItem("theme");
+	document.documentElement.removeAttribute("data-theme");
+	document.documentElement.classList.remove("dark");
+	theme();
+}
+
+
+function addSelectedTheme(targetTheme) {
+	// remove all dropdown-active classes
+	const dropdowns = document.getElementsByClassName("dropdown-item");
+	for (let i = 0; i < dropdowns.length; i++) {
+		dropdowns[i].classList.remove("dropdown-active");
+	}
+	document.getElementById("select-" + targetTheme).classList.add("dropdown-active");
+}
