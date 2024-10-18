3230e1b8325e1cfcea534cd779f9a75c4a8c8e07
Jan "JoubaMety" Rašnovský <56211361+JoubaMety@users.noreply.github.com>
2024-10-18 21:33:17 +0200

    added multiple themes support! (part 2)
    nord-dark primary was not the blue that was 'required' :skull:

--- src/script.js
+++ src/script.js
@@ -1,25 +1,3 @@
-/*
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (
-	localStorage.getItem("color-theme") === "dark" ||
-	(!("color-theme" in localStorage) &&
-		window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-	document.documentElement.classList.add("dark");
-    document.documentElement.setAttribute('data-theme', 'dark');
-} else {
-	document.documentElement.classList.remove("dark");
-    document.documentElement.setAttribute('data-theme', 'light');
-}
-
-// change src of img with id "github0" and "github1" depending on light/dark mode
-if (localStorage.theme === "light") {
-    document.documentElement.setAttribute('data-theme', 'light');
-} else {
-    document.documentElement.setAttribute('data-theme', 'dark');
-}
-	*/
-
 const themeTextMap = new Map([
 	["dark", "Catppuccin Mocha"],
 	["light", "Catppuccin Latte"],
