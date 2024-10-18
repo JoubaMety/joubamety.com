6a5b794e72f96d09d3d538f6f6d2b73716018627
Jan "JoubaMety" Rašnovský <56211361+JoubaMety@users.noreply.github.com>
2024-10-18 21:23:16 +0200

    added multiple themes support!
    holy shit, i'm dead

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+/*
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (
 	localStorage.getItem("color-theme") === "dark" ||
@@ -17,41 +18,99 @@ if (localStorage.theme === "light") {
 } else {
     document.documentElement.setAttribute('data-theme', 'dark');
 }
+	*/
 
-// change switch id "ChangeTheme" depending on light/dark mode
-const ChangeTheme = document.getElementById("ChangeTheme");
-if (localStorage.theme === "dark") {
-	ChangeTheme.checked = true;
-} else {
-	ChangeTheme.checked = false;
-}
+const themeTextMap = new Map([
+	["dark", "Catppuccin Mocha"],
+	["light", "Catppuccin Latte"],
+	["nord-dark", "Nord (Dark)"],
+	["nord-light", "Nord (Light)"],
+	["catppuccin-frappe", "Catppuccin Frappé"],
+	["catppuccin-macchiato", "Catppuccin Macchiato"],
+]);
 
-function changeIcons() {
-	const ChangeTheme = document.getElementById("ChangeTheme");
-	if (localStorage.theme === "dark") {
-		localStorage.theme = "light";
-        document.documentElement.setAttribute('data-theme', 'light');
-	} else {
-		localStorage.theme = "dark";
-        document.documentElement.setAttribute('data-theme', 'dark');
+function theme() {
+	var storedTheme =
+		localStorage.getItem("theme") ||
+		(window.matchMedia("(prefers-color-scheme: dark)").matches
+			? "dark"
+			: "light");
+	if (storedTheme) {
+		document.documentElement.setAttribute("data-theme", storedTheme);
+		changeThemeText(storedTheme);
+		addSelectedTheme(storedTheme);
 	}
 }
+theme();
+
+function changeTheme(targetTheme, ambiance) {
+	console.debug("change theme to " + targetTheme + " (" + ambiance + ")");
+	if (ambiance == "dark") {
+		document.documentElement.classList.add("dark");
+	} else {
+		document.documentElement.classList.remove("dark");
+	}
+	document.documentElement.setAttribute("data-theme", targetTheme);
+	addSelectedTheme(targetTheme);
+	localStorage.setItem("theme", targetTheme);
+	changeThemeText(targetTheme);
+}
+
+function changeThemeText(targetTheme) {
+	document.getElementById("SelectedTheme").textContent = themeTextMap.get(targetTheme);
+}
+
+// reset theme
+function resetTheme() {
+	localStorage.removeItem("theme");
+	document.documentElement.removeAttribute("data-theme");
+	document.documentElement.classList.remove("dark");
+	theme();
+}
+
+
+function addSelectedTheme(targetTheme) {
+	// remove all dropdown-active classes
+	const dropdowns = document.getElementsByClassName("dropdown-item");
+	for (let i = 0; i < dropdowns.length; i++) {
+		dropdowns[i].classList.remove("dropdown-active");
+	}
+	document.getElementById("select-" + targetTheme).classList.add("dropdown-active");
+}
+
+// preload image
+const preloadImage = (src) =>
+	new Promise((resolve, reject) => {
+		const image = new Image();
+		image.onload = resolve;
+		image.onerror = reject;
+		image.src = src;
+	});
 
 // blink, yoinked from https://github.com/sugoidogo/pngtube2/blob/master/v8/blink.html
-setTimeout(
-	()=>{
-		(function blink(){
-			const style=getComputedStyle(document.body)
-			const min=+style.getPropertyValue('--blink-min')
-			const max=+style.getPropertyValue('--blink-max')
-			const time=+style.getPropertyValue('--blink-time')
-			document.getElementById("avatar").src = style.getPropertyValue("--avatar_blink_img");
-			document.querySelector("link[rel*='icon']").href = style.getPropertyValue("--favicon_blink");
-			setTimeout(()=>{
-				document.getElementById("avatar").src = style.getPropertyValue("--avatar_img");
-				document.querySelector("link[rel*='icon']").href = style.getPropertyValue("--favicon");
-			},time*1000)
-			setTimeout(blink, (Math.random() * (max - min) + min)*1000)
-		})() // function blink
-	},2500 // delay before starting function
-) // setTimeOut
+preloadImage(
+	getComputedStyle(document.body).getPropertyValue("--avatar_blink_img")
+).then(
+	setTimeout(
+		() => {
+			(function blink() {
+				const style = getComputedStyle(document.body);
+				const min = +style.getPropertyValue("--blink-min");
+				const max = +style.getPropertyValue("--blink-max");
+				const time = +style.getPropertyValue("--blink-time");
+				document.getElementById("avatar").src =
+					style.getPropertyValue("--avatar_blink_img");
+				document.querySelector("link[rel*='icon']").href =
+					style.getPropertyValue("--favicon_blink");
+				setTimeout(() => {
+					document.getElementById("avatar").src =
+						style.getPropertyValue("--avatar_img");
+					document.querySelector("link[rel*='icon']").href =
+						style.getPropertyValue("--favicon");
+				}, time * 1000);
+				setTimeout(blink, (Math.random() * (max - min) + min) * 1000);
+			})(); // function blink
+		},
+		2500 // delay before starting function
+	) // setTimeOut
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('rippleui').Config} */
+const { createThemes } = require('tw-colors');
 module.exports = {
-  darkMode: 'selector',
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily: {
@@ -13,12 +14,12 @@ module.exports = {
   },
   plugins: [require("rippleui")],
   rippleui: {
+    /*
     themes: [
-			{
-				themeName: "light",
-				colorScheme: "light",
-        prefersColorScheme: true,
-				colors: {
+        {
+        themeName: "light", // Catppuccin Latte
+        colorScheme: "light",
+        colors: {
           primary: "#1e66f5",
           secondary: "#8839ef",
           error: "#d20f39",
@@ -32,10 +33,91 @@ module.exports = {
           content1: "#4c4f69", // Text
           content2: "#5c5f77", // Subtext 1
           content3: "#6c6f85", // Subtext 0
+        },
+      },
+      {
+				themeName: "dark", // Catppuccin Mocha
+				colorScheme: "dark",
+				colors: {
+          primary: "#89b4fa", // Blue
+          secondary: "#cba6f7", // Mauve
+          error: "#f38ba8", // Red
+          success: "#a6e3a1", // Green
+          warning: "#f9e2af", // Yellow
+          selection: "#9399b2", // Overlay 2
+
+          backgroundPrimary: "#1e1e2e", // Base
+          backgroundSecondary: "#313244", // Surface 0
+          border: "#45475a", // Surface 1
+          content1: "#cdd6f4", // Text
+          content2: "#bac2de", // Subtext 1
+          content3: "#a6adc8", // Subtext 0
 				},
 			},
+      {
+				themeName: "nord-light",
+				colorScheme: "light",
+				colors: {
+          primary: "#54B4CE", // Frost [2]
+          secondary: "#B22E9A", // Aurora [4]
+          error: "#BC2B39", // Aurora [0]
+          success: "74BC34", // Green
+          warning: "EAAD33", // Yellow
+          selection: "#D4D8E5", // Snow Storm [1] nord4
+
+          backgroundPrimary: "#ECEFF4", // Snow Storm [2] nord6
+          backgroundSecondary: "#E5E9F0", // Snow Storm [1]
+          border: "#D8DEE9", // Snow Storm [0]
+          content1: "#2E3440", // Polar Night [0]
+          content2: "#3B4252", // Polar Night [1]
+          content3: "#434C5E", // Polar Night [2]
+				},
+			},
+      {
+				themeName: "dark", // Catppuccin Mocha
+				colorScheme: "dark",
+        prefersColorScheme: true,
+				colors: {
+          primary: "#8caaee", // Blue
+          secondary: "#ca9ee6", // Mauve
+          error: "#e78284", // Red
+          success: "#a6d189", // Green
+          warning: "#e5c890", // Yellow
+          selection: "#626880", // Overlay 2
+
+          
+          backgroundPrimary: "#303446", // Base
+          backgroundSecondary: "#414559", // Surface 0
+          border: "#51576d", // Surface 1
+          content1: "#c6d0f5", // Text
+          content2: "#b5bfe2", // Subtext 1
+          content3: "#a5adce", // Subtext 0
+				},
+			},
+      
+			
 			{
-				themeName: "dark",
+				themeName: "nord-dark",
+				colorScheme: "dark",
+				colors: {
+          primary: "#8FBCBB", // Frost [1]
+          secondary: "#B48EAD", // Aurora [4]
+          error: "#BF616A", // Aurora [0]
+          success: "#A3BE8C", // Green
+          warning: "#EBCB8B", // Yellow
+          selection: "#434C5E", // Polar Night [2]
+
+          
+          backgroundPrimary: "#242933", // yoinked from nord web background
+          backgroundSecondary: "#3B4252", // Polar Night [0]
+          border: "#4C566A", // Polar Night [1]
+          content1: "#ECEFF4", // Snow Storm [2]
+          content2: "#E5E9F0", // Snow Storm [1]
+          content3: "#D8DEE9", // Snow Storm [0]
+				},
+			},
+      {
+				themeName: "catppuccin-frappe",
 				colorScheme: "dark",
 				colors: {
           primary: "#89b4fa", // Blue
@@ -54,6 +136,27 @@ module.exports = {
           content3: "#a6adc8", // Subtext 0
 				},
 			},
+      {
+				themeName: "catppuccin-macchiato",
+				colorScheme: "dark",
+				colors: {
+          primary: "#8aadf4", // Blue
+          secondary: "#c6a0f6", // Mauve
+          error: "#ed8796", // Red
+          success: "#a6da95", // Green
+          warning: "#eed49f", // Yellow
+          selection: "#939ab7", // Overlay 2
+
+          
+          backgroundPrimary: "#24273a", // Base
+          backgroundSecondary: "#363a4f", // Surface 0
+          border: "#494d64", // Surface 1
+          content1: "#cad3f5", // Text
+          content2: "#b8c0e0", // Subtext 1
+          content3: "#a5adcb", // Subtext 0
+				},
+			},
 		],
-  }
+    */
+  },
 }
